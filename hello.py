025fa06a88c4c07df6ae31f2f0f4ce97893b46ec
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2022-10-20 13:41:20 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+a = 7
+b = 5
+
+maximum = a
+if b > a :
+    maximum = b
+
+minimum = a
+if b < a :
+    minimum = b
+
+print(f"Minimum of {a}, {b} is {minimum}")
+print(f"Maximum of {a}, {b} is {maximum}")
