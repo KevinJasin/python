8ab0f0996241ae5ea2da89e77d7d4a3f6b8f2ef8
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2022-10-26 11:45:35 +0300

    Update yl9.py

--- yl9.py
+++ yl9.py
@@ -1,12 +1,15 @@
-def checkValidity(a, b, c):
-    if (a + b <= c) or (a + c <= b) or (b + c <= a) :
-        return False
-    else:
-        return True       
-a = 8
-b = 50
-c = 38
-if checkValidity(a, b, c):
-    print("Kehtiv")
+a = float(input("Sisesta 1. küljepikkus"))
+b = float(input("Sisesta 2. küljepikkus"))
+c = float(input("Sisesta 3. küljepikkus"))
+
+if a + b > c and a + c > b and b + c > a:
+   
+     if a == b == c:
+            print("Kolmnurg on võrdkülgne")
+     elif a == b or b == c or c == a:
+            print("Kolmnurg on võrdhaarne")
+     else:
+            print("Kolmnurg on erikülgne")
+
 else:
-    print("Kehtetu")
+     print("Selliste küljepikkustega kolmnurk ei saa eksisteerida")
