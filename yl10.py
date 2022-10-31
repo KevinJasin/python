c1302581d52b4cde11e965cf86f8d4cb3605151a
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2022-10-31 13:06:35 +0200

    Update yl10.py

--- yl10.py
+++ yl10.py
@@ -1,6 +1,12 @@
-name = input("Mis su nimi on:")
+name = input("Mis su nimi on: ")
 print("Tere", name)
-home = input("Kus sa elad:")
-if home == ("Saaremaa")
-flag = "Tere Saarlane"
-
+home = input("Kus sa elad: ")
+if (home) == ("Saaremaa"):
+      print("Tere saarlane")
+age = int(input("Mis su vanus on: "))
+if age < 18:
+     print("kasutaja on liiga noor et kasutada autod")
+elif age == 18:
+    print("Suurepärane sa oled 18, sa võid kasutada autod")
+else:
+     print("võid autod juhtida")
