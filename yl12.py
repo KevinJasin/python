13ab6076439dfc4a90cff9b022da2fd9081ea616
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2022-10-31 13:42:53 +0200

    Add files via upload

---
+++ yl12.py
@@ -0,0 +1,12 @@
+fruitlist = ["Õun", "Viinamari", "Arbuus"]
+print(fruitlist[0])
+fruitlist.append("laim")
+print(fruitlist)
+
+fruitlist[2] = "Pirn"
+print(fruitlist)
+print(len(fruitlist))
+fruitlist.remove("Viinamari")
+print(fruitlist)
+fruitlist.reverse()
+print(fruitlist)
