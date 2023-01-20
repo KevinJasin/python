b38fcbafcb67b04324e2fd0f214de016b3650d3f
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2023-01-20 11:45:08 +0200

    Create yl26_input_data.py

---
+++ yl26_input_data.py
@@ -0,0 +1,11 @@
+sales_results = {
+    "Johnver": {
+        "revenue": {
+            "tea": 190,
+            "coffe": 325,
+            "water": 682,
+            "milk": 829
+        },
+        "expenses"
+     }
+}
