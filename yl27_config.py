1cd21028b10fea84d8c4ccbf8a798a5adcb5f56b
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2023-02-06 11:39:55 +0200

    Create yl27_config.py

---
+++ yl27_config.py
@@ -0,0 +1,6 @@
+
+config = {
+    "client_id": "C0-aUFUQiC58MZ4C2X-avA",
+    'client_secret':"TH-9VeO1iYuNG7FTRy2ubQCbPQYtCA",
+    'user_agent': "python:C0-aUFUQiC58MZ4C2X-avA:v1 (by u/jikroy0)",
+}
