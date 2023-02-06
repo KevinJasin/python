4bc6ea157eaecdf3d7b1d96657ac0987b3eb9718
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2023-02-06 11:39:02 +0200

    Update yl27.py

--- yl27.py
+++ yl27.py
@@ -2,12 +2,6 @@ import praw
 import sys
 import os
 
-reddit = praw.Reddit(
-    client_id="C0-aUFUQiC58MZ4C2X-avA",
-    client_secret="TH-9VeO1iYuNG7FTRy2ubQCbPQYtCA",
-    user_agent="python:C0-aUFUQiC58MZ4C2X-avA:v1 (by u/jikroy0)",
-)
-
 current = os.path.dirname(os.path.realpath(_file_))
 parent = os.path.dirname(current)
 sys.path.append(parent)
@@ -15,8 +9,8 @@ sys.path.append(parent)
 from yl27_config import yl27_config
 
 reddit = praw.Reddit(
-    client_id = config["client_id"]
-    client_secret = config["client_secret"]
-    user_agent = config["user_agent"]
+    client_id = config["client_id"],
+    client_secret = config["client_secret"],
+    user_agent = config["user_agent"],
 )
 
