abe0decf5a8ce1b8daf265f1860b903103c1fd4f
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2023-02-08 08:33:30 +0200

    Delete yl27_config.py

--- yl27_config.py
+++
@@ -1,6 +0,0 @@
-
-config = {
-    "client_id": "C0-aUFUQiC58MZ4C2X-avA",
-    'client_secret':"TH-9VeO1iYuNG7FTRy2ubQCbPQYtCA",
-    'user_agent': "python:C0-aUFUQiC58MZ4C2X-avA:v1 (by u/jikroy0)",
-}
